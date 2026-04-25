90c6577e64e3d70159ad6f6fb26de2bc07b5af9f
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2026-04-25 22:29:56 +0100

    Change search bar aesthetically

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 // import logo from "../src/assets/images/logo.png"
 import React from "react";
-import {Badge, Button, Input, Table} from "reactstrap";
+import {Badge, Button, Input, InputGroup, InputGroupText, Table} from "reactstrap";
 import bellIcon from '../assets/bell.svg'
 
 const Header = () => {
@@ -8,24 +8,40 @@ const Header = () => {
         <header>
             <div id="top-header">
                 <Table>
-                    <th style={{textAlign: "left"}}>
-                        <h3>Bom dia,</h3>
-                        <h4>Visitante!</h4>
-                    </th>
-                    <th style={{textAlign: "right"}}>
-                        <div id="notification-area">
-                            <Button color="primary" outline>
-                                <img src={bellIcon} alt="Bell Icon" />{' '}
-                              <Badge>4</Badge>
-                            </Button>
-                        </div>
-                    </th>
+                    <thead>
+                        <tr>
+                            <th style={{textAlign: "left"}}>
+                                <h3>Bom dia,</h3>
+                                <h4>Visitante!</h4>
+                            </th>
+                            <th style={{textAlign: "right"}}>
+                                <div id="notification-area">
+                                    <Button color="primary" outline>
+                                        <img src={bellIcon} alt="Bell Icon" />{' '}
+                                      <Badge>4</Badge>
+                                    </Button>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
                 </Table>
             </div>
             <div id="searchbar">
-              <Input/>
+              <SearchBox/>
             </div>
         </header>
     )
 }
+
+const SearchBox = () => {
+    return (
+        <InputGroup>
+            <InputGroupText>
+                <i className="bi bi-search"/>
+            </InputGroupText>
+            <Input placeholder="Search..."/>
+        </InputGroup>
+    );
+}
+
 export default Header;
