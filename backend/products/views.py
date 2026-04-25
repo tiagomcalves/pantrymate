b50cf4f949f7d22cffbef7c8dec22282e44bf032
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2026-04-25 20:54:46 +0100

    Set up initial API for Products App

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import *  # (1)
 
-# Create your views here.
+
+@api_view(['GET'])
+def products(request):
+    if request.method == 'GET':
+        products_list = Product.objects.all()
+        serializer = ProductsSerializer(products_list, many=True)
+        return Response(serializer.data)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
