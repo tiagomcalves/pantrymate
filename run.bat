3d1e83f00af121510b79d111750870042e1d3e22
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2026-04-27 22:51:31 +0100

    Disable echoes from run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-start "PantryMate Backend" /I /D backend python manage.py runserver
+@echo off
+echo Launching Frontend and Backend services
 start "PantryMate Frontend" /D frontend cmd /c "npm run dev"
+start "PantryMate Backend" /I /D backend python manage.py runserver
