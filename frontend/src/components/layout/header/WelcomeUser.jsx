132075f5af689543681f15df5b786a891f46f397
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2026-05-06 01:16:47 +0100

    Add badge to Welcome User message

--- frontend/src/components/layout/header/WelcomeUser.jsx
+++ frontend/src/components/layout/header/WelcomeUser.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import {Badge} from "reactstrap";
 
 const WelcomeUser = () => {
 
@@ -17,7 +18,12 @@ const WelcomeUser = () => {
     return (
         <>
             <h3>Bom dia,</h3>
-            <h4>{strUsername}!</h4>
+            <div style={{ display: "flex"}} >
+                <h4>{strUsername}!</h4>
+                <div style={{margin: "5px 20px"}}>
+                    <Badge color="success" pill style={{ height: "22px"}}>status</Badge>
+                </div>
+            </div>
         </>
     );
 }
