b4f8a8cc1885144fab6654e3b355fa63e44eb01c
Ruben <rmhra@iscte-iul.pt>
2026-05-11 21:22:21 +0100

    Fixed merge sorry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './pages/Landpage.jsx'
+import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<Home/>)
+createRoot(document.getElementById('root')).render(<App/>)
