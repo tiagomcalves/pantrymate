1e3629a6d6bbe21ec8d82f41f1b6b028dcb3eb65
Ruben-ISCTE <rmhra@iscte-iul.pt>
2026-05-21 21:50:23 +0100

    Frontend receitas v1

--- backend/products/views.py
+++ backend/products/views.py
@@ -18,16 +18,10 @@ def products(request):
 @api_view(['GET', 'POST'])
 def itens_dispensa(request):
     if request.method == 'GET':
-        familia = None
-        if request.user.is_authenticated:
-            membro = MembroFamilia.objects.filter(user=request.user).first()
-            if membro:
-                familia = membro.family
-        if not familia:
-            familia = Familia.objects.first()
-        if not familia:
+        membro = MembroFamilia.objects.filter(user=request.user).first()
+        if not membro:
             return Response([])
-        itens = ItemDispensa.objects.filter(familia=familia)
+        itens = ItemDispensa.objects.filter(familia=membro.family)
         serializer = ItemDispensaSerializer(itens, many=True)
         return Response(serializer.data)
 
